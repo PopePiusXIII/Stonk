6090d4310464b15fc0f83906a2f7bb05333d4f1d
PopePiusXIII <harleyh@knights.ucf.edu>
2022-01-04 11:04:25 -0500

    Update stock.py
    fixed a lot of where the data is scraped. free cash flow calculation was checked against cameron stewart cfa msft video and is accurate

--- stock.py
+++ stock.py
@@ -6,26 +6,37 @@ class Stock:
     def __init__(self, ticker, company_name):
         self._yfin_data = yf.Ticker(ticker)
         self.balance_sheet = mts.get_financial_sheet(ticker, company_name, 'balance-sheet')
+        self.balance_sheet.replace(r'^\s*$', 0, regex=True, inplace=True)
+
         self.income_statement = mts.get_financial_sheet(ticker, company_name, 'income-statement')
-        self.cashflow_sheet = mts.get_financial_sheet(ticker, company_name, 'cash-flow-statement')
-        self.pe = mts.get_table(ticker, company_name, 'pe-ratio').iloc[:, -1]
-        self.pfcf = mts.get_table(ticker, company_name, 'price-fcf').iloc[:, -1]
+        self.income_statement.replace(r'^\s*$', 0, regex=True, inplace=True)
         self.ebitda = self.income_statement.iloc[16, 1:].astype(float)
         self.eps = self.income_statement.iloc[21, 1:].astype(float)
         self.revenue = self.income_statement.iloc[0, 1:].astype(float)
-        self.net_cash_flow = self.cashflow_sheet.iloc[26, 1:].astype(float)
+        self.shares_outstanding = self.income_statement.iloc[19, 1:].astype(float)
+
+        self.cashflow_sheet = mts.get_financial_sheet(ticker, company_name, 'cash-flow-statement')
+        self.cashflow_sheet.replace(r'^\s*$', 0, regex=True, inplace=True)
         self.net_income = self.cashflow_sheet.iloc[0, 1:].astype(float)
         self.depreciation_amortization = self.cashflow_sheet.iloc[1, 1:].astype(float)
         self.other_noncash_items = self.cashflow_sheet.iloc[2, 1:].astype(float)
-        self.capex = self.cashflow_sheet.iloc[17, 1:].astype(float)
-        self.shares_outstanding = self.income_statement.iloc[19, 1:].astype(float)
+        self.cash_from_operating = self.cashflow_sheet.iloc[9, 1:].astype(float)
+        self.capex = self.cashflow_sheet.iloc[10, 1:].astype(float)
+        self.debt_issuance_retirement_net = self.cashflow_sheet.iloc[20, 1:].astype(float)
+        self.free_cash_flow_to_equity = self.capex + self.cash_from_operating + self.debt_issuance_retirement_net
+
+        self.pe = mts.get_table(ticker, company_name, 'pe-ratio').iloc[:, -1]
+        self.pfcf = mts.get_table(ticker, company_name, 'price-fcf').iloc[:, -1]
+
         self.market_price = self._yfin_data.info['bid']
         self.enterprise_value = self._yfin_data.info['enterpriseValue'] / 10**6
+
         self.capital_change = 0
         self.owner_earnings_per_share = (self.net_income + self.depreciation_amortization + self.other_noncash_items +
                                          self.capex + self.capital_change) / self.shares_outstanding
+        self.free_cash_flow_to_equity_per_share = self.free_cash_flow_to_equity / self.shares_outstanding
         self.price_owners_earnings = self.market_price / self.owner_earnings_per_share
-        self.evebitda = self.enterprise_value / self.ebitda
+        self.ev_ebitda = self.enterprise_value / self.ebitda
 
 
 if __name__ == "__main__":
